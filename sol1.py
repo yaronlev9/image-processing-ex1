c3db8dea9e09c32b8193e619b3b6c85102f2a0c8
yaronlev9 <62477626+yaronlev9@users.noreply.github.com>
2020-10-30 00:39:43 +0200

    added question 2

--- sol1.py
+++ sol1.py
@@ -9,6 +9,7 @@ NUM_OF_PIXELS = 255
 # x = np.hstack([np.repeat(np.arange(0,50,2),10)[None,:], np.array([255]*6)[None,:]])
 # grad = np.tile(x,(256,1))
 # print(x)
+# imageio.imwrite("before.jpg", grad)
 
 def read_image(filename, representation):
     im = imageio.imread(filename)
@@ -20,5 +21,16 @@ def read_image(filename, representation):
     elif len(im.shape) < 3 and representation == 1:
         return im / NUM_OF_PIXELS
     else:
-        return 'error'
+        raise()
+
+def imdisplay(filename, representation):
+    try:
+        img = read_image(filename, representation)
+    except:
+        return 'wrong input'
+    plt.figure()
+    plt.imshow(img, cmap="gray")
+    plt.show()
+
+# imdisplay('low_contrast.jpg', 1)
 
